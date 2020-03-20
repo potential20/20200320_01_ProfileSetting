b82f53024e4edbaa12214a38f3c8806b6577a475
potential20 <passion212@nate.com>
2020-03-20 11:17:16 +0900

    선택한 이미지 경로 로그로 찍기까지.

--- app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.a20200320_01_profilesetting;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -12,6 +13,8 @@ import android.view.View;
 import kr.co.tjoeun.a20200320_01_profilesetting.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    private static final int REQUEST_FOR_ALBUM = 1000;
 
     ActivityMainBinding binding = null;
 
@@ -41,7 +44,7 @@ public class MainActivity extends BaseActivity {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
                 intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                startActivity(intent);
+                startActivityForResult(intent,REQUEST_FOR_ALBUM);
             }
         });
 
@@ -50,5 +53,18 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupValues() {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_FOR_ALBUM) {
+            if (resultCode == RESULT_OK) {
+                if (data.getData() != null) {
+                    Log.d("사진가져오기",data.getData().toString());
+                }
+            }
+        }
     }
 }
