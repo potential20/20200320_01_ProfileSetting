d86ccf6ebf6d35c4fda40794e1278cff1f7870dd
potential20 <passion212@nate.com>
2020-03-20 10:58:39 +0900

    프로필 사진 누르면 => 사진 선택 화면으로 이동

--- app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
@@ -3,8 +3,11 @@ package kr.co.tjoeun.a20200320_01_profilesetting;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 
 import kr.co.tjoeun.a20200320_01_profilesetting.databinding.ActivityMainBinding;
 
@@ -28,6 +31,19 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.profilePicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                갤러리에서 사진을 가져오자!
+//                안드로이드가 제공하는 화면(갤러리) => Intent로 이동.
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivity(intent);
+            }
+        });
 
     }
 
