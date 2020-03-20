34644751073f70156ebc76aefefcd94d08a3c91b
potential20 <passion212@nate.com>
2020-03-20 09:52:36 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.a20200320_01_profilesetting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupEvents();
+        setupValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setupValues() {
+
+    }
+}
