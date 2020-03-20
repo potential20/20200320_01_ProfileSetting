33e75d11d52ffe2dcb725cae18034a6c627e48c3
potential20 <passion212@nate.com>
2020-03-20 10:16:35 +0900

    데이터바인딩 준비

--- app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_01_profilesetting/MainActivity.java
@@ -1,15 +1,20 @@
 package kr.co.tjoeun.a20200320_01_profilesetting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kr.co.tjoeun.a20200320_01_profilesetting.databinding.ActivityMainBinding;
+
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         setupEvents();
         setupValues();
     }
